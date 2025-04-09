5739928ff03dfbcfa13fbe2a6e531db188fd6ea7
VinayakNPN <chouhanvinayak86@gmail.com>
2025-04-09 20:04:47 +0530

    Login Page changed

--- login_script.js
+++ login_script.js
@@ -1,19 +1,19 @@
-const container = document.getElementById('container');
-const registerBtn = document.getElementById('register');
-const loginBtn = document.getElementById('login');
+// const container = document.getElementById('container');
+// const registerBtn = document.getElementById('register');
+// const loginBtn = document.getElementById('login');
 
-registerBtn.addEventListener('click', () => {
-    container.classList.add("active");
-});
+// registerBtn.addEventListener('click', () => {
+//     container.classList.add("active");
+// });
 
-loginBtn.addEventListener('click', () => {
-    container.classList.remove("active");
-});
+// loginBtn.addEventListener('click', () => {
+//     container.classList.remove("active");
+// });
 
-function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-}
+// function onSignIn(googleUser) {
+//     var profile = googleUser.getBasicProfile();
+//     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+//     console.log('Name: ' + profile.getName());
+//     console.log('Image URL: ' + profile.getImageUrl());
+//     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+// }
